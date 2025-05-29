c1bb2a36106e4508b1579841635a609a06fa0599
DungeonDesk <samuel.richards.09@gmail.com>
2025-05-29 13:22:14 -0500

    initial setup

---
+++ src/main/java/net/dungeondesk/qualitytools/QualityToolsClient.java
@@ -0,0 +1,10 @@
+package net.dungeondesk.qualitytools;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class QualityToolsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
