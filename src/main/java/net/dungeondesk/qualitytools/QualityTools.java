8ffcfe20ab6df7736d47fbb2433867213e952412
DungeonDesk <samuel.richards.09@gmail.com>
2025-05-29 13:37:01 -0500

    initial setup

---
+++ src/main/java/net/dungeondesk/qualitytools/QualityTools.java
@@ -0,0 +1,16 @@
+package net.dungeondesk.qualitytools;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class QualityTools implements ModInitializer {
+	public static final String MOD_ID = "qualitytools";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/dungeondesk/qualitytools/QualityToolsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.dungeondesk.qualitytools;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class QualityToolsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
